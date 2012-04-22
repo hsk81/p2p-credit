fde5ef68ac63b6f7658644a68233859dcebb9fab
Hasan Karahan <hasan.karahan81@gmail.com>
2012-04-22 19:13:51 +0200

    added serve control script and ignoring pid files

---
+++ p2p-credit/serve.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PROJDIR="$(pwd)"
+PROJECT="$(basename $PROJDIR)"
+
+SRVMETH="threaded"
+HOSTVAL="${2-127.0.0.1}"
+PORTVAL="${3-3002}"
+PIDFILE="$PROJDIR/$PROJECT.pid"
+
+case "$1" in
+	start)
+        if [ -f $PIDFILE ]; then
+            PID=$(cat -- $PIDFILE)
+            STA=$(ps $PID)
+            if [ "$?" -eq "0" ] ; then
+                exit 0
+            fi
+            rm -f -- $PIDFILE
+        fi
+        exec /usr/bin/env - PYTHONPATH="../python:.." \
+            ./manage.py runfcgi method=$SRVMETH host=$HOSTVAL port=$PORTVAL \
+                pidfile=$PIDFILE
+        ;;
+    stop)
+        if [ -f $PIDFILE ]; then
+            kill $(cat -- $PIDFILE) &> /dev/null
+            rm -f -- $PIDFILE
+        fi
+        ;;
+    restart)
+		$0 stop
+		$0 start
+        ;;
+    status)
+        if [ -f $PIDFILE ]; then
+            PID=$(cat -- $PIDFILE)
+            STA=$(ps $PID)
+            if [ "$?" -eq "0" ] ; then
+                echo "live: $PID"
+            else
+                echo "dead: $PID"
+            fi
+        else
+            echo "dead"
+        fi
+        ;;
+    *)
+		$0 restart
+esac
+
+exit 0
